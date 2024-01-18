e7bb22a060bc4b88e93a39caafdfa04d830790da
AkshayaRobbi <149946045+AkshayaRobbi@users.noreply.github.com>
2024-01-18 21:30:18 +0530

    Create script.js

---
+++ amazon prime page/script.js
@@ -0,0 +1,9 @@
+function login() {
+  // Add your authentication logic here
+  alert('Login button clicked. Add your authentication logic.');
+}
+
+function forgotPassword() {
+  // Add your "Forgot Password" logic here
+  alert('Forgot Password link clicked. Add your logic.');
+}
